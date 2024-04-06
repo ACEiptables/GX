45624e2e36f3c05b4240aeaa39504c84111c3355
ACEiptables <164225517+ACEiptables@users.noreply.github.com>
2024-04-06 15:21:20 +0800

    Update GXGJF.sh

--- GXGJF.sh
+++ GXGJF.sh
@@ -1,12 +1,12 @@
 echo "\033[32m\n检查更新中\033[0m"
 
-sleep 0.5
+
 FILE=/data/GJF/9999
 if [ ! -f "$FILE" ]; then
 echo "Hello, World!" > /data/GJF/9999
     echo "\033[34m\n自动更新中\033[0m"
     curl -o /data/GJF/1.0 "https://mirror.ghproxy.com/https://raw.githubusercontent.com/ACEiptables/GX/main/国际版本"
-sleep 2
+
     echo "⚠️⚠️⚠️⚠️⚠️⚠️"
     echo "\033[35m设备厂商:\033[0m"
 getprop ro.product.brand
