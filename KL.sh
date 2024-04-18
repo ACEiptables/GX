f24d0325d473d2b7190850003d869a9c7e58d365
ACEiptables <164225517+ACEiptables@users.noreply.github.com>
2024-04-18 09:05:28 +0800

    Add files via upload

--- KL.sh
+++ KL.sh
@@ -10,6 +10,7 @@ iptables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 80 -j REJECT
 iptables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 80 -j REJECT
 ip6tables -I OUTPUT -m owner --uid-owner=$uid -p tcp --dport 80 -j REJECT
 ip6tables -I OUTPUT -m owner --uid-owner=$uid -p udp --dport 80 -j REJECT
+echo -e "\033[5;46;42;31m            【 小叽猪 】                 \033[0m"
 sleep 5
 iptables -F 
 iptables -X 
